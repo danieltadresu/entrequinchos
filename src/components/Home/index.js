3bb3bdc20c42c62f494bef262024a06f193b6aed
danieltadresu <danieldev1999@gmail.com>
2021-07-25 01:24:23 -0400

    update: add transitons

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
@@ -7,37 +7,52 @@ import PropTypes from "prop-types";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    margin: "2rem auto"
+    margin: "2rem auto",
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
-    transition: 'all 0.3s ease-out',
+    transition: "all 0.3s ease-out",
   },
   modalOpen: {
     opacity: 1,
-    transform: 'translateY(0)'
+    transform: "translateY(0)",
   },
   modalClosed: {
     opacity: 0,
-    transform: 'translateY(-100%)'
+    transform: "translateY(-100%)",
   },
 }));
 
 const Home = ({ visible }) => {
   const classes = useStyles();
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
-    console.log(`visible`, visible);
-  }, [visible])
+    // console.log(`visible`, visible);
+    setAnimate(visible);
+  }, [visible]);
+
+  useEffect(() => {
+    // setVisible(true);
+    setAnimate((prevState) => !prevState);
+  }, []);
 
   return (
     <div className={classes.root}>
       <Toolbar>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <Paper className={classes.paper}>xs=12 lg=6</Paper>
+            <Paper
+              className={
+                animate
+                  ? `${classes.paper} ${classes.modalOpen}`
+                  : `${classes.paper} ${classes.modalClosed}`
+              }
+            >
+              xs=12 lg=6
+            </Paper>
           </Grid>
         </Grid>
       </Toolbar>
